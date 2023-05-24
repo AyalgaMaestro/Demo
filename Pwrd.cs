ee42e62f5f676d918310499b4d93f2c68a9a5679
AyalgaMaestro <134511964+AyalgaMaestro@users.noreply.github.com>
2023-05-24 23:21:37 +0700

    Add files via upload

---
+++ Pwrd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PPPanda
+{
+    public partial class Pwrd : Form
+    {
+        public Pwrd()
+        {
+            InitializeComponent();
+            this.FormClosing += Pwrd_FormClosing;
+        }
+
+        private void Pwrd_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Owner.Show();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "update auth set pass = '" + textBox2.Text + "' where login = '" + textBox1.Text + "';";
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("Server = localhost; database = ppanda; user = root; password = root;");
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+                cmd.ExecuteReader();
+                conn.Close();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
